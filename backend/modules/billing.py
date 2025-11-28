7d78aa754142bed77b9ac754f6b3e20dcffe4214
Yassin Alam <yassin.alam@gmail.com>
2025-11-29 00:59:58 +0200

    Create billing module with invoice items and bill generation for the invoice (patient)

---
+++ backend/modules/billing.py
@@ -0,0 +1,43 @@
+class InvoiceItem:
+    '''
+    InvoiceItem class is used to represent one service or product in the bill
+    For Example: X-Ray, quantity 1, price 300
+    '''
+
+    def __init__(self, description, quantity, price):
+        self.description = description
+        self.quantity = quantity
+        self.price = price
+
+    # Return Price of this item
+    def get_subtotal(self):
+        return self.quantity * self.price
+    
+
+class Invoice:
+    
+    def __init__(self, invoice_id, patient_name):
+        self.invoice_id = invoice_id
+        self.patient_name = patient_name
+        self.items = []
+
+    def add_item(self, description, quantity, price):
+        item = InvoiceItem(description, quantity, price)
+        self.items.append(item)
+
+    def get_total(self):
+        total = 0
+        for item in self.items:
+            total += item.get_subtotal()
+        return total
+    
+    # Generating the bill for the patient
+    def generate_bill(self):
+        print(f"---------BILL-------")
+        print(f"Patient ID : {self.invoice_id}")
+        print(f"Patient: {self.patient_name}")
+        print("Items: ")
+        for item in self.items:
+            print(f" - {item.description} x{item.quantity} = {item.get_subtotal()}")
+        print(f"Total bill: {self.get_total()}")
+        print("----------------------")
