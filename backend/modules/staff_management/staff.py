b9b30c6bb4b28e90a5774d1cf683c6ccee3f5171
Yassin Mahmoud <yassin.alam@gmail.com>
2025-12-01 11:41:47 +0200

    Moved the place of Doctor Class in staff.py to a separate file doctor.py

--- backend/modules/staff_management/staff.py
+++ backend/modules/staff_management/staff.py
@@ -83,34 +83,6 @@ class Staff:
                 
         print(f"--> Saved {self.name} to {filename}")
 
-class doctor(Staff):   ##inheritance#
-    
-    def __init__(self, id, name, age, password, department, salary, specialization):
-    
-        super().__init__(id, name, age, password, department, salary)
-
-        self.specialization = specialization
-        self.patient_list = []
-
-    def view_dashboard(self):      # POLYMORPHISM Overriding the parent method
-                                                    
-        print(f"\n=== doctor PORTAL ===")
-        print(f"Dr. {self.name} ({self.specialization})")
-        print(f"Assigned Patients: {len(self.patient_list)}")
-        print("1. View Schedule")
-        print("2. Prescribe Medication")
-
-    def add_patient(self, patient_name):
-        self.patient_list.append(patient_name)
-        print(f"Patient {patient_name} added to Dr. {self.name}'s list.")
-
-    def prescribe_medication(self, patient_name, medicine):
-        if patient_name in self.patient_list:
-            print(f"Prescribed {medicine} to {patient_name}.")
-        else:
-            print(f"Error: {patient_name} is not your patient.")
-
-
 class nurse(Staff):   
    
     def __init__(self, id, name, age, password, department, salary, shift):
