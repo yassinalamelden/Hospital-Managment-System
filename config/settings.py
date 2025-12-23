2b1151087089e8a6811551a35c7ad3e9de6f491c
Yassin Mahmoud <yassin.alam@gmail.com>
2025-12-23 11:16:41 +0200

    Initialize Django project settings and integrate custom application modules.

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,4 @@
 
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -11,7 +10,7 @@ USE_SQL_SERVER = True
 
 SECRET_KEY = 'django-insecure-+kxppgr^&r4^(4mffy+l%pp(o$_=)h&hb^^((qli&dte*mjtvw'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['LAPTOP-KC3LAFMF', 'localhost', '127.0.0.1', '*']
 
 
 # Application definition
@@ -65,7 +64,7 @@ if USE_SQL_SERVER:
         'default': {
             'ENGINE': 'mssql',
             'NAME': 'hospital_db',
-            'HOST': r'(localdb)\MSSQLLocalDB',
+            'HOST': r'LAPTOP-KC3LAFMF',
             'PORT': '',
             'OPTIONS': {
                 'driver': 'ODBC Driver 17 for SQL Server',
@@ -100,113 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_TZ = True
-
-
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-
-
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-USE_SQL_SERVER = True
-
-
-SECRET_KEY = 'django-insecure-+kxppgr^&r4^(4mffy+l%pp(o$_=)h&hb^^((qli&dte*mjtvw'
-DEBUG = True
-ALLOWED_HOSTS = []
-
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    # Custom Apps
-    'accounts',
-    'operations',
-    'billing',
-    'core',
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-ROOT_URLCONF = 'config.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'config.wsgi.application'
-
-
-
-if USE_SQL_SERVER:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'mssql',
-            'NAME': 'hospital_db',
-            'HOST': r'(localdb)\MSSQLLocalDB',
-            'PORT': '',
-            'OPTIONS': {
-                'driver': 'ODBC Driver 17 for SQL Server',
-                'extra_params': 'Trusted_Connection=yes',
-            },
-        }
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-
-
-# Password validation
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
